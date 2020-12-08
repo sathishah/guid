6eca758957791687c914b5d3cdeccbd4d10b33c2
sathishah <sathish.ahs11@gmail.com>
2020-12-08 20:57:01 +0530

    Removed spaces

--- guid.service.ts
+++ guid.service.ts
@@ -1,26 +1,28 @@
 class uuid extends Uint16Array {
+	
 	constructor() {
-		super(16)
+		super(16);
 		/* not v4, just some random bytes */
 		window.crypto.getRandomValues(this);
 	}
+	
 	public generateGUID(): string | null {
-		let id = new String()
+		let guID = new String();
 		for (let i = 0; i < this.length; i++) {
 			/*convert uint16 to hex string */
-			let hex = this[i].toString(16).toUpperCase()
+			let hex = this[i].toString(16).toUpperCase();
 
 			/*add zero padding*/
 			while (hex.length < 2) {
-				hex = String(0).concat(hex)
+				hex = String(0).concat(hex);
 			}
-			id += hex
+			guID += hex;
 
 			/* add dashes */
 			if (i == 4 || i == 6 || i == 8 || i == 10 || i == 16){
-				id += '-'
+				guID += '-';
 			}
 		}
-		return id
+		return guID;
 	}
 }
